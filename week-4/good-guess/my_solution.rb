0195ae5803d2c6eee644a51cb31877106fe4d977
regina <reginawong14@gmail.com>
2015-11-11 15:58:43 -0800

    complete good guess

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,17 @@
+# Good Guess
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def good_guess?(n)
+	if n == 42
+		true
+	else
+		false
+	end
+end
+
+good_guess?(42)
+good_guess?(40)
---
+++ week-4/my_solution.rb
@@ -0,0 +1,10 @@
+def good_guess?(n)
+	if n == 42
+		true
+	else
+		false
+	end
+end
+
+good_guess?(42)
+good_guess?(40)
