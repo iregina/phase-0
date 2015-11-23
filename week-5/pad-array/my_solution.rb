2e5ccba21b1cac9ce87daf34acb718d206e68ed1
regina <reginawong14@gmail.com>
2015-11-23 13:26:02 -0500

    ruby array

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -64,13 +64,6 @@ end
 #refactored solution 
 
 
-
-
-
-
-
-
-
 def pad!(array, min_size, value = nil) #destructive
  # Your code here
  if min_size <= array.size
@@ -114,3 +107,28 @@ def pad(array, min_size, value = nil)
     new_array <<value while new_array.length < min_size
     new_array
 end 
+
+
+
+=begin
+Reflection: 
+
+
+Were you successful in breaking the problem down into small steps?
+Yes! It was far simpler once we started breaking it down into small steps.
+
+Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+We had some difficultires working with the new methods and understanding what was destructive or not. Luckily, our friends in SF gave us a tip to start a new array, and that helped us a lot. 
+
+Was your initial solution successful at passing the tests? If so, why do you think that is? 
+If not, what were the errors you encountered and what did you do to resolve them?
+We had errors where our method didn't work. 
+
+When you refactored, did you find any existing methods in Ruby to clean up your code?
+We did! 
+
+How readable is your solution? Did you and your pair choose descriptive variable names?
+Very readable. We did choose a descriptive variable name! 
+
+What is the difference between destructive and non-destructive methods in your own words?
+A Destructive method changes the input whereas the non-destructive method would keep the original input in tact. 
