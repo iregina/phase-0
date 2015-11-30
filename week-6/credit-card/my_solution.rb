81e93a75537bfdb641eb0d31e08f53f355f8c426
regina <reginawong14@gmail.com>
2015-11-30 09:47:11 -0800

    update paired programming session

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -33,3 +33,43 @@ class CreditCard
   
   end
 end
+
+=begin
+Pseudocode:
+Take number move to string
+Split string into individual digits
+Reverse (or run code from the back of the input)
+back to integer
+
+double every second digit
+back to string 
+split individual digits
+back to integer 
+add digits 
+modulo 10 if true then it's valid
+
+problems we still need to solve, 
+break double digits, 
+remove spaces from original string
+=end
+
+
+=begin
+
+REFLECTION TIME:
+
+What was the most difficult part of this challenge for you and your pair?
+The most difficult part was communicating ideas on how to solve this idea
+Luckily, we took things one step at a time and it became more clear how to solve
+certain problems.
+
+
+What new methods did you find to help you when you refactored?
+I learned more about how to use .inject(:+) . join .split .map! .map!.with_index
+
+
+What concepts or learnings were you able to solidify in this challenge?
+I'm beginning to feel more comfortable with pseudocoding and breaking donw
+algorithms one step a time. 
+  
+=end
