7e3ae4cc7bba5c1ae0ef1895bb7f631d65a268f0
regina <reginawong14@gmail.com>
2015-12-07 11:08:22 -0800

    game

---
+++ week-7/game.js
@@ -0,0 +1,71 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description: 
+// Overall mission: To make a cake.
+// Goals: Make the batter, decorate the cake 
+// Characters: Me
+// Objects: Cake, Frosting
+// Functions: Mix Ingredients Bake the cake frost the cake
+
+// Pseudocode
+// 1. Combine all the ingredients for baking a cake in a mixing bowl 
+// 2. Mix the ingredient
+// 5. Place pan in oven 
+// 6. Take out cake and cool it 
+// 7. Mix the ingredients for frosting
+// 8. Frost the cake
+
+// Initial Code
+var pantry = {
+	batter = ["flour", "eggs", "milk", "baking powder"]
+	frosting = ["powdered sugar", "whipping cream", "vanilla"]
+}
+
+var supplies = {
+	oven: "gas"
+	bowl: "mixing"
+	pan: "12-inch"
+}
+
+var mix = function(ingredient){
+	return "you just combined" + " " + ingredient;
+}
+
+var cook = function(tool){
+	return "you just placed your cake in the" + " " + tool;
+}
+
+var frost = function(){
+	return "you just frosted your cake"
+}
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+// What was the most difficult part of this challenge?
+// I still don't understand how to build a game like this and need to look at more examples. 
+
+// What did you learn about creating objects and functions that interact with one another?
+// It's quite exciting! 
+// Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
+// Not in this challenge
+// How can you access and manipulate properties of objects?
+// you have to put down object.ingredient in that type of syntax 
+//
+//
+//
+//
+//
+//
+//
